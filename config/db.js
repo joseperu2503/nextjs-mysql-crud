e091b142d331ed88f7ca945897ab2b2203809ac0
joseperu2503 <51133263+joseperu2503@users.noreply.github.com>
2022-11-18 01:10:27 -0500

    commit

--- config/db.js
+++ config/db.js
@@ -1,11 +1,17 @@
 import { createPool } from 'mysql2/promise'
 
+// const pool = createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'junior2503',
+//   database: 'crud-next'
+// })
+
 const pool = createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'junior2503',
-    // port: 8080,
-    database: 'crud-next'
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DATABASE,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD
 })
 
 export { pool }
